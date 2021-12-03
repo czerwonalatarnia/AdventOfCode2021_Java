2a81bc88d62fddb0a7a5576929d0f52c4afd5638
czerwonalatarnia <15816253+czerwonalatarnia@users.noreply.github.com>
2021-12-04 00:30:09 +0100

    Fixes of fixes.

--- src/test/java/aoc2021/Day02/Day02Test.java
+++ src/test/java/aoc2021/Day02/Day02Test.java
@@ -1,16 +1,15 @@
 package aoc2021.Day02;
 
+import java.io.IOException;
+import java.nio.file.Path;
 import aoc2021.own.exception.FileIsEmpty;
 import aoc2021.own.functions.DataReader;
 import org.junit.jupiter.api.Test;
-
-import java.io.IOException;
-import java.nio.file.Path;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class Day02Test {
 	public static final Path resourcePath = Path.of("src", "test", "resources", "aoc2021", "Day02");
+
 	@Test
 	void testPart1() throws FileIsEmpty, IOException {
 		var movementTest = DataReader.readAlchemyString(resourcePath.resolve("day02test.txt"));
