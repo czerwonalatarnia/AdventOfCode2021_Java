182b9783cfa5e8b917c0998e202b7e933aeee9a6
Michał Zagórski <zagura6@gmail.com>
2021-12-04 22:57:58 +0100

    Revert whitespace change in Da01 test

--- src/test/java/aoc2021/Day01/Day01Test.java
+++ src/test/java/aoc2021/Day01/Day01Test.java
@@ -8,12 +8,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class Day01Test {
 	public static final Path resourcePath = Path.of("src", "test", "resources", "aoc2021", "Day01");
 
-
 	@Test
 	void testPart1() {
 		var sonarReports = DataReader.readLongArray(resourcePath.resolve("day01test.txt"));
 		assertEquals(7, new Day01().part1(sonarReports));
 	}
+
 	@Test
 	void testPart2() {
 		var sonarReports = DataReader.readLongArray(resourcePath.resolve("day01test.txt"));
