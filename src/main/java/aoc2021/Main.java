2af91a33fbb30badc60de60ed823c98e930a8eb4
czerwonalatarnia <15816253+czerwonalatarnia@users.noreply.github.com>
2021-12-05 13:10:40 +0100

    Added a greeting to main function.

--- src/main/java/aoc2021/Main.java
+++ src/main/java/aoc2021/Main.java
@@ -8,8 +8,9 @@ public class Main {
 
 		/* If you want to run specific day, set value of DAY to the day you want and MONTH to 12. */
 
+		System.out.println("Welcome to Advent of Code 2021!\n");
 		int DAY = LocalDate.now(ZoneId.of("Europe/Warsaw")).getDayOfMonth();
 		int MONTH = LocalDate.now(ZoneId.of("Europe/Warsaw")).getMonthValue();
-		RunHelper.runChallenges(3, MONTH);
+		RunHelper.runChallenges(DAY, MONTH);
 	}
 }
