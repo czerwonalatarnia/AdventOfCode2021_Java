f7f0711f716cdbc2951ed1219324a7f2887461a6
czerwonalatarnia <15816253+czerwonalatarnia@users.noreply.github.com>
2021-12-10 07:05:19 +0100

    Day 10 solution v1.0.

--- src/main/java/aoc2021/Day10/Day10.java
+++ src/main/java/aoc2021/Day10/Day10.java
@@ -6,15 +6,60 @@ import aoc2021.own.functions.DataReader;
 
 public class Day10 implements IDay {
 	public void day() {
-		LinkedList<Long> sonarReports = DataReader.readLongArray(DataReader.createFilePath(10));
-		System.out.println("Day 10\nThe answer to part 1 is " + part1() + "\nThe answer to part 2 is " + part2());
+		LinkedList<String> data = DataReader.readAlchemyString(DataReader.createFilePath(10));
+		System.out.println("Day 10\nThe answer to part 1 is " + part1(data) + "\nThe answer to part 2 is " + part2(data));
 	}
 
-	int part1() {
-		return 0;
+	long part1(LinkedList<String> data) {
+		long sum = 0;
+		for (int lines = 0; lines < data.size(); lines++) {
+			String line = data.get(lines);
+			char nextProblem = ' ';
+			line = connectBrackets(line);
+			for (int it = 1; it < line.length(); it++) {
+				if (line.charAt(it) == ')' || line.charAt(it) == '}' || line.charAt(it) == ']' || line.charAt(it) == '>') {
+					nextProblem = line.charAt(it);
+					data.remove(lines);
+					lines--;
+					break;
+				}
+			}
+			switch (nextProblem) {
+				case ')' -> sum += 3;
+				case ']' -> sum += 57;
+				case '}' -> sum += 1197;
+				case '>' -> sum += 25137;
+			}
+		}
+		return sum;
 	}
 
-	int part2() {
-		return 0;
+	long part2(LinkedList<String> data) {
+		LinkedList<Long> sum = new LinkedList<>();
+		for (var line : data) {
+			line = connectBrackets(line);
+			long sumLine = 0;
+			for (int it = line.length() - 1; it >= 0; it--) {
+				switch (line.charAt(it)) {
+					case '(' -> sumLine = sumLine * 5 + 1L;
+					case '[' -> sumLine = sumLine * 5 + 2L;
+					case '{' -> sumLine = sumLine * 5 + 3L;
+					case '<' -> sumLine = sumLine * 5 + 4L;
+				}
+			}
+			sum.add(sumLine);
+		}
+		sum.sort(Long::compareTo);
+		return sum.get(sum.size() / 2);
+	}
+
+	private String connectBrackets(String line) {
+		for (int it = 1; it < line.length(); it++) {
+			if (Math.abs((line.charAt(it) - line.charAt(it - 1))) <= 2 && (line.charAt(it) > line.charAt(it - 1)) && (line.charAt(it) != line.charAt(it - 1))) {
+				line = line.substring(0, it - 1) + line.substring(it + 1);
+				it = 0;
+			}
+		}
+		return line;
 	}
 }
--- src/test/java/aoc2021/Day10/Day10Test.java
+++ src/test/java/aoc2021/Day10/Day10Test.java
@@ -2,23 +2,20 @@ package aoc2021.Day10;
 
 import java.nio.file.Path;
 import aoc2021.own.functions.DataReader;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@Disabled
 class Day10Test {
 	public static final Path resourcePath = Path.of("src", "test", "resources", "aoc2021", "Day10");
 
-	@Test
-	void testPart1() {
+	@Test void testPart1() {
 		var dataTest = DataReader.readAlchemyString(resourcePath.resolve("day10test.txt"));
-		assertEquals(0, new Day10().part1());
+		assertEquals(26397L, new Day10().part1(dataTest));
 	}
 
-	@Test
-	void testPart2() {
+	@Test void testPart2() {
 		var dataTest = DataReader.readAlchemyString(resourcePath.resolve("day10test.txt"));
-		assertEquals(0, new Day10().part2());
+		new Day10().part1(dataTest);
+		assertEquals(288957L, new Day10().part2(dataTest));
 	}
 }
