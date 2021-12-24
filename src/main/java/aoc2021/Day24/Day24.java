e1fb4b60443c62fa4f1f7f817f9a5175141a9df6
czerwonalatarnia <15816253+czerwonalatarnia@users.noreply.github.com>
2021-12-25 00:58:10 +0100

    Day 24: v0.-1: had working test unit to check if some positions don't matter. Then switched from int to long, and it has stopped working.

--- src/main/java/aoc2021/Day24/Day24.java
+++ src/main/java/aoc2021/Day24/Day24.java
@@ -7,100 +7,152 @@ import aoc2021.own.functions.DataReader;
 public class Day24 implements IDay {
 	public void day() {
 		LinkedList<String> data = DataReader.readAlchemyString(DataReader.createFilePath(24));
+		ALUTestUnit(data);
 		System.out.println(
 				"Day 24\nThe answer to part 1 is " + part1(data) + "\nThe answer to part 2 is " + part2(data));
 	}
 
 	long part1(LinkedList<String> data) {
-		int[] dimensions = new int[]{0, 0, 0, 1};
-		long monad = 100_000_000_000_000L;
+		long[] dimensions = new long[]{0, 0, 0, 1};
+		// monad need to be 7 digits long
+		long monad = 10_000_000L;
 		String MONAD;
-		int from;
-		int to;
-		while (dimensions[3] != 0) {
-			dimensions = new int[]{0, 0, 0, 0};
-			int error = 0;
-			int charAt = 0;
+		while (dimensions[3] != 0 && monad > 1_111_111) {
+			dimensions = new long[]{0, 0, 0, 0};
 			monad--;
-			MONAD = String.valueOf(monad);
-			while (MONAD.contains("0")) {
+			MONAD = getMonadString(monad);
+			while (MONAD.contains("0") && monad >= 1_111_111) {
 				monad--;
-				MONAD = String.valueOf(monad);
+				MONAD = getMonadString(monad);
 			}
-			System.out.println(monad);
-			for (var line : data) {
-				switch (line.substring(0, 3)) {
-					case "inp" -> {
-						switch (line.charAt(4)) {
-							case 'w' -> dimensions[0] = MONAD.charAt(charAt) - '0';
-							case 'x' -> dimensions[1] = MONAD.charAt(charAt) - '0';
-							case 'y' -> dimensions[2] = MONAD.charAt(charAt) - '0';
-							case 'z' -> dimensions[3] = MONAD.charAt(charAt) - '0';
-						}
-						charAt++;
-					}
-					case "add" -> {
-						to = getTo(line);
-						from = getFrom(line);
-						if (from < 0)
-							dimensions[to] += Integer.parseInt(line.substring(6));
-						else
-							dimensions[to] += dimensions[from];
-					}
-					case "mul" -> {
-						to = getTo(line);
-						from = getFrom(line);
-						if (from < 0)
-							dimensions[to] *= Integer.parseInt(line.substring(6));
-						else
-							dimensions[to] *= dimensions[from];
-					}
-					case "div" -> {
-						to = getTo(line);
-						from = getFrom(line);
-						if (from < 0)
-							from = Integer.parseInt(line.substring(6));
-						else
-							from = dimensions[from];
-						if (from == 0)
-							error = -1;
-						else
-							dimensions[to] /= from;
-					}
-					case "mod" -> {
-						to = getTo(line);
-						from = getFrom(line);
-						if (from < 0)
-							from = Integer.parseInt(line.substring(6));
-						else
-							from = dimensions[from];
-						if (from <= 0)
-							error = -22;
-						else if (dimensions[to] < 0)
-							error = -21;
-						else
-							dimensions[to] /= from;
-					}
-					case "eql" -> {
-						to = getTo(line);
-						from = getFrom(line);
-						if (from < 0)
-							dimensions[to] = dimensions[to] == Integer.parseInt(line.substring(6)) ? 1 : 0;
-						else
-							dimensions[to] = dimensions[to] == dimensions[from] ? 1 : 0;
-					}
-				}
-				if (error < 0)
-					break;
-			}
-			if (error < 0)
+			if (!ALU(data, dimensions, MONAD))
 				dimensions[3] = 1;
+			else
+				System.out.println(dimensions[3]);
 		}
-		return monad;
+		System.out.println(monad);
+		return Long.parseLong(getMonadString(monad));
 	}
+
+	// All dependencies on each position of the string were found out by ALUTestUnit and ALUTesting.
 
 	long part2(LinkedList<String> data) {
 		return 0;
+	}
+
+	private String getMonadString(long monad) {
+		// different: 1, 2, 3, 6, 7, 10, 14
+		// the same: 4, 5, 8, 9, 11, 12, 13
+		String monadString = String.valueOf(monad);
+		return String.valueOf(monadString.charAt(0)) + monadString.charAt(1) + monadString.charAt(2) + "99" +
+		       monadString.charAt(3) + monadString.charAt(4) + "99" + monadString.charAt(5) + "999" +
+		       monadString.charAt(6);
+	}
+
+	private boolean ALU(LinkedList<String> data, long[] dimensions, String MONAD) {
+		int charAt = 0;
+		for (var line : data) {
+			switch (line.substring(0, 3)) {
+				case "inp" -> {
+					input(dimensions, MONAD, charAt, line);
+					charAt++;
+				}
+				case "add" -> addition(dimensions, line);
+				case "mul" -> multiply(dimensions, line);
+				case "div" -> {
+					if (!division(dimensions, line))
+						return false;
+				}
+				case "mod" -> {
+					if (!modulo(dimensions, line))
+						return false;
+				}
+				case "eql" -> equals(dimensions, line);
+				default -> {
+					System.out.println("ERROR ERROR ERROR ERROR");
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	private void input(long[] dimensions, String MONAD, int charAt, String line) {
+		switch (line.charAt(4)) {
+			case 'w' -> dimensions[0] = MONAD.charAt(charAt) - '0';
+			case 'x' -> dimensions[1] = MONAD.charAt(charAt) - '0';
+			case 'y' -> dimensions[2] = MONAD.charAt(charAt) - '0';
+			case 'z' -> dimensions[3] = MONAD.charAt(charAt) - '0';
+		}
+	}
+
+	private void addition(long[] dimensions, String line) {
+		int from;
+		int to;
+		to = getTo(line);
+		from = getFrom(line);
+		if (from < 0)
+			dimensions[to] += Long.parseLong(line.substring(6));
+		else
+			dimensions[to] += dimensions[from];
+	}
+
+	private void multiply(long[] dimensions, String line) {
+		int from;
+		int to;
+		to = getTo(line);
+		from = getFrom(line);
+		if (from < 0)
+			dimensions[to] *= Long.parseLong(line.substring(6));
+		else
+			dimensions[to] *= dimensions[from];
+	}
+
+	private boolean division(long[] dimensions, String line) {
+		int to;
+		int from;
+		long substitute;
+		to = getTo(line);
+		from = getFrom(line);
+		if (from < 0)
+			substitute = Long.parseLong(line.substring(6));
+		else
+			substitute = dimensions[from];
+		if (from == 0)
+			return false;
+		else
+			dimensions[to] /= substitute;
+		return true;
+	}
+
+	private boolean modulo(long[] dimensions, String line) {
+		int from;
+		int to;
+		long substitute;
+		to = getTo(line);
+		from = getFrom(line);
+		if (from < 0)
+			substitute = Long.parseLong(line.substring(6));
+		else
+			substitute = dimensions[from];
+		if (from <= 0)
+			return false;
+		else if (dimensions[to] < 0)
+			return false;
+		else
+			dimensions[to] /= substitute;
+		return true;
+	}
+
+	private void equals(long[] dimensions, String line) {
+		int from;
+		int to;
+		to = getTo(line);
+		from = getFrom(line);
+		if (from < 0)
+			dimensions[to] = dimensions[to] == Long.parseLong(line.substring(6)) ? 1 : 0;
+		else
+			dimensions[to] = dimensions[to] == dimensions[from] ? 1 : 0;
 	}
 
 	private int getTo(String line) {
@@ -123,5 +175,62 @@ public class Day24 implements IDay {
 			case 'z' -> from = 3;
 		}
 		return from;
+	}
+
+	void ALUTestUnit(LinkedList<String> data) {
+		long[] dimensions;
+		long counter = 1;
+		LinkedList<String> testingUnit = new LinkedList<>();
+		testingUnit.add("91111111111111");
+		testingUnit.add("19111111111111");
+		testingUnit.add("11911111111111");
+		testingUnit.add("11191111111111");
+		testingUnit.add("11119111111111");
+		testingUnit.add("11111911111111");
+		testingUnit.add("11111191111111");
+		testingUnit.add("55582558858885");
+		testingUnit.add("55599559959395");
+		testingUnit.add("11199119919991");
+		for (var el : testingUnit) {
+			dimensions = new long[]{0, 0, 0, 0};
+			System.out.println(counter + ": " + ALUTesting(data, dimensions, el));
+			counter++;
+		}
+		/*long monad = 28_347_585_311_110L;
+		boolean error = true;
+		while (error && monad < 100_000_000_000_000L) {
+			error = false;
+			dimensions = new long[]{0, 0, 0, 0};
+			monad++;
+			while (String.valueOf(monad).contains("0")) {
+				monad++;
+			}
+			if (!ALU(data, dimensions, String.valueOf(monad)))
+				error = true;
+		}*/
+	}
+
+	private long ALUTesting(LinkedList<String> data, long[] dimensions, String MONAD) {
+		int charAt = 0;
+		for (var line : data) {
+			switch (line.substring(0, 3)) {
+				case "inp" -> {
+					input(dimensions, MONAD, charAt, line);
+					charAt++;
+				}
+				case "add" -> addition(dimensions, line);
+				case "mul" -> multiply(dimensions, line);
+				case "div" -> {
+					if (division(dimensions, line))
+						System.out.println("\tDividing error");
+				}
+				case "mod" -> {
+					if (modulo(dimensions, line))
+						System.out.println("\tModulo error");
+				}
+				case "eql" -> equals(dimensions, line);
+			}
+		}
+		return dimensions[3];
 	}
 }
