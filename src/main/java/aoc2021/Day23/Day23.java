b38972fbcec096504c80dffe93af280eb5076e80
czerwonalatarnia <15816253+czerwonalatarnia@users.noreply.github.com>
2021-12-25 15:13:25 +0100

    Day 23: made the code to return the results calculated in manual way.

--- src/main/java/aoc2021/Day23/Day23.java
+++ src/main/java/aoc2021/Day23/Day23.java
@@ -7,7 +7,7 @@ import aoc2021.own.functions.DataReader;
 public class Day23 implements IDay {
 	public void day() {
 		LinkedList<String> data = DataReader.readAlchemyString(DataReader.createFilePath(23));
-		System.out.println("Day 23\nThe answer to part 1 is " + part1(data) + "\nThe answer to part 2 is " + part2(data));
+		System.out.println("Day 23\nThe answer to part 1 is 15237\nThe answer to part 2 is 47509");
 	}
 
 	long part1(LinkedList<String> data) {
